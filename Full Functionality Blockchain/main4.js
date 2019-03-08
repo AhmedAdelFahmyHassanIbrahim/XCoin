94358c45de5a7535cb181232488e890b0aff65b3
Ahmed Adel Fahmy Hassan <ahmed.hedia@outlook.com>
2019-03-08 21:06:29 +0200

    Blockchain simple model

---
+++ Full Functionality Blockchain/main4.js
@@ -0,0 +1,23 @@
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const {
+    Blockchain,
+    Transaction
+} = require('./Blockchian');
+
+const myKey = ec.keyFromPrivate('474fe3cebc5183bab892dd31fbda80a0ec957b598823d23e462c9d015f884bb1');
+const myWalletAddress = myKey.getPublic('hex');
+
+
+let savjeeCoin = new Blockchain();
+
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+savjeeCoin.addTransaction(tx1);
+
+
+console.log("\n starting the miner ...");
+savjeeCoin.miningPendingTransactions(myWalletAddress);
+
+
+console.log("\n Balance of Xavier is " + savjeeCoin.getBalanceOfAddress(myWalletAddress));
